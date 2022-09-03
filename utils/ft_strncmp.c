c927b0eba703851fd50c3be35cd719534924c663
Abdelhamid Bouazi [Opus] <Abdelhamid01bouazi@gmail.com>
2022-09-03 18:08:34 +0100

    start by builtins

---
+++ utils/ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouazi <abouazi@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 13:09:38 by abouazi           #+#    #+#             */
+/*   Updated: 2022/09/03 17:53:52 by abouazi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if ((!s1 || !s2) && !n)
+		return (0);
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
